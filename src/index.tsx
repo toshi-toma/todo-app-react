a8891c053cafbd5b2ae6142cb683134f43b32760
toshi-toma <toshitoma1217@gmail.com>
2019-08-12 15:19:43 +0900

    Setting up a React + TypeScript + Storybook + styled-components

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import styled from "styled-components";
 
 import App from "./App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+
+const Button = styled.button`
+  font-size: 1.5em;
+  text-align: center;
+  color: #3498db;
+  background-color: #f7f9fa;
+  border: 1px solid #e3e7e8;
+  cursor: pointer;
+  :hover {
+    background-color: #c8d6dd;
+  }
+`;
+
+export default Button;

50b1ed365d0fc4bfe6897d6c3e770a626d4d68e1
toshi-toma <toshitoma1217@gmail.com>
2019-08-12 15:43:58 +0900

    ignore rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,9 @@
 module.exports = {
-  extends: "airbnb-typescript-prettier"
+  extends: "airbnb-typescript-prettier",
+  rules: {
+    "import/no-extraneous-dependencies": ["error", {
+      devDependencies: ["**/*.stories.tsx"],
+      peerDependencies: false
+    }]
+  }
 };

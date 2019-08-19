8bdbc123138da1610ba35efee5b5cfcad2a58769
toshi-toma <toshitoma1217@gmail.com>
2019-08-19 19:53:58 +0900

    add webpack settings

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,6 @@
-import styled from "styled-components";
+import React from "react";
+import ReactDOM from "react-dom";
 
-const Button = styled.button`
-  font-size: 1.5em;
-  text-align: center;
-  color: #3498db;
-  background-color: #f7f9fa;
-  border: 1px solid #e3e7e8;
-  cursor: pointer;
-  :hover {
-    background-color: #c8d6dd;
-  }
-`;
+import App from "./App";
 
-export default Button;
+ReactDOM.render(<App />, document.getElementById("root"));

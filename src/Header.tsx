45751ceb24ec46b9fd8be9479c3c649ffcdc8dc5
toshi-toma <toshitoma1217@gmail.com>
2020-03-17 00:32:41 +0900

    change title

--- src/Header.tsx
+++ src/Header.tsx
@@ -3,7 +3,10 @@ import styled from "styled-components";
 
 import InputForm from "./Form/InputForm";
 
-const Head = styled.header``;
+const Head = styled.header`
+  background-color: #fff;
+  color: #000;
+`;
 
 const H1 = styled.h1``;
 
@@ -14,7 +17,7 @@ interface Props {
 
 const Header: FC<Props> = ({ onChange, inputText }) => (
   <Head>
-    <H1>ToDo App</H1>
+    <H1>A-Day</H1>
     <InputForm onChange={onChange} placeholder="Your Todos" value={inputText} />
   </Head>
 );
